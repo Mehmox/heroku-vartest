8258503c85cf4f086b51c49b92ca777fce2326cc
Mehmox <71236327+Mehmox@users.noreply.github.com>
2024-09-10 14:12:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({ extended: true }))
 
-app.listen(port, () => console.log(`lisenning on port: ${port}`))
+app.listen(port, () => console.log(`Lisenning on port: ${port}`))
 
 app.get("/", async (req, res) => {
-    // let newData = new data({ comment: "this is my first fake data to test my db" })
-    // await newData.save()
-    // const allData = await data.find({})
-    // res.render("index", { allData })
+    let newData = new data({ comment: "this is my first fake data to test my db" })
+    await newData.save()
+    const allData = await data.find({})
+    res.render("index", { allData })
     // console.log(allData)
     // res.render("test", { e })
 })
