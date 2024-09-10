d1e9d897e1f35346a5af5e9672348e3c4a4146e5
Mehmox <71236327+Mehmox@users.noreply.github.com>
2024-09-10 13:48:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,30 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 3000
-let uri = "mongodb://localhost:27017/test_db_connection";
+const port = process.env.PORT || 3000;
+// let uri = "mongodb://localhost:27017/test_db_connection";
+let uri = "";
 let e = "no error";
-mongoose.connect(uri)
-    .then((r) => {
-        console.log("MONGO CONNECTED!!")
-    })
-    .catch((err) => {
-        console.log("MONGO ERROR");
-        console.log(err);
-        e = err
-    });
+// mongoose.connect(uri)
+//     .then((r) => {
+//         console.log("MONGO CONNECTED!!")
+//     })
+//     .catch((err) => {
+//         console.log("MONGO ERROR");
+//         console.log(err);
+//         e = err
+//     });
 
 // const Schema = new mongoose.Schema({
 //     comment: String,
 // })
 // const data = mongoose.model("Comment", Schema)
+try {
+    if (testvar === 1)
+        e = "connect"
+} catch (error) {
+    e = "test var not found"
+}
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
