4a5bf4495f4f07055beb800d59a922b8170eda05
Mehmox <71236327+Mehmox@users.noreply.github.com>
2024-09-10 14:00:02 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 3000;
+let e = "no error";
 // let uri = "mongodb://localhost:27017/test_db_connection";
 // let uri = "";
-let e = "no error";
 // mongoose.connect(uri)
 //     .then((r) => {
 //         console.log("MONGO CONNECTED!!")
@@ -20,6 +20,7 @@ let e = "no error";
 //     comment: String,
 // })
 // const data = mongoose.model("Comment", Schema)
+
 // try {
 //     const testvar = process.env.TESTVAR; // Environment variable'ı al
 //     if (testvar === '1') // Değeri kontrol et
